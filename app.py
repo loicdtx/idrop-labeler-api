db7c6e736d4d8cff81d8c92418f48ad6030c4aa9
Loïc Dutrieux <loic.dutrieux@gmail.com>
2019-04-12 14:55:58 +0200

    wrote all api resources

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from jsonschema import validate
 
 import idb
 from idb.db import session_scope
+from idb.utils import snakify
 
 app = Flask(__name__)
 
@@ -23,6 +24,57 @@ def not_found(error):
 def get_species():
     with session_scope() as session:
         return jsonify({'species': idb.species(session)})
+
+
+@app.route('/idrop/v0/studyareas', methods = ['GET'])
+def get_studyareas():
+    with session_scope() as session:
+        return jsonify({'species': idb.studyareas(session)})
+
+
+@app.route('/idrop/v0/inventories', methods = ['GET'])
+def get_inventories():
+    content = request.get_json(silent=True)
+    with session_scope() as session:
+        if content is None:
+            # Use default parameters
+            fc = idb.inventories(session=session)
+        # Convert camelCase dict keys to snake_case
+        params = {snakify(k):v for k,v in content.items()}
+        # TODO: Implement json validation
+        # try:
+            # validate(content, SEARCH_SCHEMA)
+        # except Exception as e:
+            # abort(400)
+        fc = idb.inventories(session=session, **params)
+    return jsonify(fc)
+
+
+@app.route('/idrop/v0/interpreted', methods=['POST'])
+def post_interpreted():
+    content = request.get_json(silent=True)
+    # TODO: implement proper json validation
+    if content is None:
+        abort(400)
+    feature = {snakify(k):v for k,v in content.items()}
+    with session_scope() as session:
+        # add_interpreted returns a list, hence the s
+        inserted_features = idb.add_interpreted(session=session, fc=feature)
+    return jsonify({'interpretedId': inserted_features[0]['id']}), 201
+
+
+@app.route('/idrop/v0/interpreted', method=['GET'])
+def get_interpreted():
+    content = request.get_json(silent=True)
+    with session_scope() as session:
+        if content is None:
+            # Use default parameters
+            fc = idb.interpreted(session=session)
+        params = {snakify(k):v for k,v in content.items()}
+        fc = idb.interpreted(session=session, **params)
+    return jsonify(fc)
+
+
 
 
 if __name__ == '__main__':
