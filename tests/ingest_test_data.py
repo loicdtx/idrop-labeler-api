75cc8faa51b431895699182df3f0a6a815170c29
Loïc Dutrieux <loic.dutrieux@gmail.com>
2019-04-16 21:58:22 +0200

    Transfered test data and ingestion script from idb to here

---
+++ tests/ingest_test_data.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import fiona
+
+from idb.db import session_scope, init_db
+from idb import add_inventories
+from idb.models import Tile, Studyarea, Species
+from idb.utils import get_or_create
+
+
+csv_file = 'data/species.csv'
+gpkg_file = 'data/test_data.gpkg'
+
+
+# Create database tables
+init_db()
+
+# Ingest species
+with open(csv_file) as src:
+    reader = csv.reader(src)
+    with session_scope() as session:
+        for row in reader:
+            get_or_create(session=session, model=Species,
+                          code=row[0], name=row[1])
+
+# Add tiles
+with fiona.open(gpkg_file, layer='tiles') as src:
+    tile_list = [Tile.from_geojson(feature) for feature in src]
+with session_scope() as session:
+    session.add_all(tile_list)
+
+# Add inventory samples
+with fiona.open(gpkg_file, layer='inventory') as src:
+    with session_scope() as session:
+        add_inventories(session, list(src))
+
+# Add study area
+with fiona.open(gpkg_file, layer='studyarea') as src:
+    studyarea_list = [Studyarea.from_geojson(feature) for feature in src]
+with session_scope() as session:
+    session.add_all(studyarea_list)
+
